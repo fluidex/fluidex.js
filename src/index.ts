8bb0086756873561b0ff983b8131581d138bceef
Steven Gu <asongala@163.com>
2021-08-18 12:44:29 +0000

    Exports type TransferTx.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { get_CREATE_L2_ACCOUNT_MSG, Account, TxSignature } from './account';
 import { hash } from './hash';
 import { OrderSide, OrderInput, OrderState } from './order';
 import { TransferTx } from './transfer';
-export { get_CREATE_L2_ACCOUNT_MSG, Account, hash, OrderState, OrderInput, OrderSide, TxSignature };
+export { get_CREATE_L2_ACCOUNT_MSG, Account, hash, OrderState, OrderInput, OrderSide, TransferTx, TxSignature };
